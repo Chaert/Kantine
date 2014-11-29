32b46045d347b4e72610e29f973f39ba6e7b89c6
Daniël Boonstra <daniel.boonstra@outlook.com>
2014-11-29 12:09:28 +0100

    Paar nieuwe wijzigingen

--- Kantine.java
+++ Kantine.java
@@ -3,6 +3,8 @@ public class Kantine {
   private KassaRij kassarij;
   public Persoon persoon;
   public Dienblad dienblad;
+  public Artikel artikel1;
+  public Artikel artikel2;
 
   /**
    * Constructor
@@ -20,34 +22,41 @@ public class Kantine {
    * Tenslotte sluit de Persoon zich aan bij de rij 
    * voor de kassa.
    */
-  public void loopPakSluitAan() {
-    //omitted
+  public void loopPakSluitAan(Persoon persoon, Artikel artikel1, Artikel artikel2) {
+    this.persoon = persoon;
+    dienblad = new Dienblad();
+    persoon.pakDienblad(dienblad);
+    this.artikel1 = artikel1;
+    this.artikel2 = artikel2;
+    persoon.pakArtikel(artikel1);
+    persoon.pakArtikel(artikel2);
+    kassarij.sluitAchteraan(persoon);
   }
 
   /**
    * Deze methode handelt de rij voor de kassa af.
    */
   public void verwerkRijVoorKassa() {
-    while() {
+    //while() {
       //omitted   
-    }
+    //}
   }
 
   /**
    * Deze methode telt het geld uit de kassa
    * @return hoeveelheid geld in kassa
    */
-  public double hoeveelheidGeldInKassa() {
+  //public double hoeveelheidGeldInKassa() {
     //omitted
-  }
+  //}
 
   /**
    * Deze methode geeft het aantal gepasseerde artikelen.
    * @return het aantal gepasseerde artikelen
    */
-  public int aantalArtikelen(){
+  //public int aantalArtikelen(){
     //omitted
-  }
+  //}
 
   /**
    * Deze methode reset de bijgehouden telling van 
